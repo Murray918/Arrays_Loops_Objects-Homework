2e4e4970affe70ec09937e107a878789ad40b379
Jacob Werbin <jwerbin95@gmail.com>
2018-12-13 16:59:50 -0600

    Commit 2 - Get Even answered

--- app.js
+++ app.js
@@ -2,3 +2,18 @@ for(let i = 1;i<=20;i++)
 {
 	console.log(i)
 }
+for(let i = 0;i<=200;i++)
+{
+	if(i%2==0)
+		console.log(i)
+}
+for(let i = 0;i<20;i++)
+{
+	let stringArray = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."]
+	if(i%2==0)
+	{
+		console.log(stringArray[Math.floor(Math.random() * 2)])
+	}
+	else
+		console.log("Love me, pet me! HSSSSSS!")
+}
