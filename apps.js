412823669126a61abfdb57d32add2e01c0ad1a92
Sithiz <Joshklaas1@gmail.com>
2018-12-13 21:03:23 -0600

    Excited Kittens answered

--- apps.js
+++ apps.js
@@ -1,3 +1,4 @@
+/*
 for(let i = 1 ; i <= 20; i++){
 	console.log(i)
 }
@@ -7,3 +8,25 @@ for(let i = 0; i <=200; i++){
 		console.log(i)
 	}
 }
+*/
+let getCatSaying = function(max) {
+	return  Math.floor(Math.random() * Math.floor(max))
+}
+
+let catSayings = ["...human...why you taking pictures of me?...","...the catnip made me do it...",
+"...why does the red dot always get away..."]
+
+for(let x = 0;x <=20; x++){
+	if(x%2 !=1){
+		console.log(catSayings[getCatSaying(3)])
+		} else {
+			console.log('Love ne, pet me! HSSSSS!')
+		}
+
+
+	}
+
+
+
+
+console.log(getCatSaying(3))
