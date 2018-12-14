cea0e0c31d017d4afb61fe188f2b69e9b5f7130a
Julius Camarillo <julcamarillo17@gmail.com>
2018-12-13 23:01:00 -0600

    Commit 3 Excited Kittens answered

--- app.js
+++ app.js
@@ -7,3 +7,10 @@ for (i=0; i<200;i++){
 	if (i % 2 ===0) 
 	console.log(i)
 }
+//3,	
+for (j=0; j<20; j++){
+	console.log("Love me, pet me!HSSSSSS")
+	if (j % 1 ===0){
+		console.log("...human...why are you taking pictures of me?")
+	}
+	}
