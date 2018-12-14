01b5fb704a5aafff54d700707498e11e86388451
Jamal Amar <JamalAmar@Jamals-MacBook-Air.local>
2018-12-13 18:29:16 -0600

    while loop created

--- app.js
+++ app.js
@@ -4,8 +4,19 @@ for(let i=1; i<=100; i++) {
 }
 */
 
+
+/*
 for(let i=0; i<=200; i++) {
 	if ((i%2) == 0) {
 		console.log(i)
 	}
+}
+*/
+
+
+let i = 1;
+
+while(i <= 20) {
+	console.log("Love me, pet me! HSSSSSS!")
+	i++
 }
