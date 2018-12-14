afb3728140d86d965476b71b0c46afe7c703434e
Tristan Bennett <tristanbennett@tristanova.local>
2018-12-14 01:40:11 -0600

    Commit 4 - Fizz Buzz answered

--- app.js
+++ app.js
@@ -31,3 +31,22 @@
 // 		} else {console.log("Math.random() error")} // confirmed something is slipping through my Math.random() usage
 // 	} else {console.log("Love me, pet me! HSSSSSS!");}
 // }
+
+// FizzBuzz 
+
+// 1. Write a javascript application that logs all numbers from 1 - 100.
+// 2. If a number is divisible by 3 log "Fizz" instead of the number.
+// 3. If a number is divisible by 5 log "Buzz" instead of the number.
+// 4. If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
+
+// for (let i=1; i<= 100; i++) {
+// 	if (i % 3 === 0 && i % 5 === 0) {
+// 		console.log("FizzBuzz");
+// 	} else if (i % 3 === 0) {
+// 		console.log("Fizz");
+// 	} else if (i % 5 === 0) {
+// 		console.log("Buzz");
+// 	} else console.log(i);
+// }
+
+
