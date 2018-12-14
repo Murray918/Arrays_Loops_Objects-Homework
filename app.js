f33b12c6553d821de7d736c7aa92a85490b236a4
Johnathan <reddjohnathan@gmail.com>
2018-12-13 19:11:56 -0600

    Excited Kittens answered

--- app.js
+++ app.js
@@ -5,16 +5,22 @@
 for(let i = 1; i <= 20; i++){
 	console.log(i)
 }
-*/
+
 
 //Write a for loop that will log only the even numbers in 0 through 200.
-
-//for(let i = 0; i % 0; i++){
-
-//}
 
 for(let i = 1; i <=200; i++){
 	if(i % 2 == 0){
 		console.log(i)
 	}
+}
+*/
+
+
+for(let kitten = 0; kitten < 20; kitten++){
+	if(kitten % 2 == 0){
+	console.log("Love me, pet me! HSSSSSS!")
+  } else{
+  	console.log("...human...why you taking pictures of me?...")
+  }
 }
