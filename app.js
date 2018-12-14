7157c74aea9e6c9b2a832755c576a58fcb743531
ohheyitslance <ohheyitslance@ohheyitsMacBook.attlocal.net>
2018-12-13 23:50:04 -0600

    commit 4 - fizzbuzz answered

--- app.js
+++ app.js
@@ -4,3 +4,30 @@ for (i = 0; i <= 20; i++)
 // 2.
 for (i = 0; i <= 200; i+=2)
 	console.log(i)
+// // 3.
+// let cat = ["human, why are you taking pictures of me?", "the catnip made me do it", "why does the red dot always get away"]
+// for (i = 0; i <= 20; i++)
+// 	if (i % 2 == 0){
+// 		console.log(cat(Math.random))
+// 	}
+// 	else console.log("love me, pet me! HSSSSSS!")
+// 4.
+for (i = 0; i <= 100 ; i++)
+	if (i % 3 == 0 && i % 5 == 0) {
+		console.log("FizzBuzz")
+	}
+	else if (i % 5 == 0) { 
+		console.log("buzz")
+	}
+	else if (i % 3 == 0) 
+		console.log("Fizz")
+	else {
+		console.log(i)
+	}
+// 5.
+// 6.
+// 7.
+// 8.
+// 9.
+// 10.
+// 11.
