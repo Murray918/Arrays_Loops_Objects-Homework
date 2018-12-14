0b0193dfc485abc206441751d6df5c8623b93fee
Sithiz <Joshklaas1@gmail.com>
2018-12-13 22:07:31 -0600

    Getting to know you answered

--- apps.js
+++ apps.js
@@ -25,7 +25,7 @@ for(let x = 0;x <=20; x++){
 	}
 */
 
-
+/*
 
 for(let i = 1; i<=100; i++){
 	if(i%3 ==0 && i%5 == 0){
@@ -37,7 +37,40 @@ for(let i = 1; i<=100; i++){
 	}else {
 		console.log(i)
 	}
-}
+}*/
+const kenny = ["Kenny", 1000, "Austin"];
+const jimHaff = ["Jim H", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const jimClark = ["Jim C", 186, "LA"];
+const ryan = ["Ryan", 65, "Denver"];
+
+kenny[0] = 'Gameboy'
+
+jimClark[1] = jimClark[1] + 1
+
+ryan[2] = 'Gotham City'
+
+reuben.pop()
+reuben.push('chicago')
+
+jimHaff.pop()
+jimHaff.push('houston','austin','dallas')
+
+jimHaff.splice(2,1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
