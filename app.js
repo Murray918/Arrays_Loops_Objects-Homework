71650ccf06a6c5eff9d2a0500c2d947ec73f7b89
Johnathan <reddjohnathan@gmail.com>
2018-12-14 08:42:15 -0600

    Getting to Know You answered

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ for(let i = 1; i <=200; i++){
 		console.log(i)
 	}
 }
-*/
+
 
 
 for(let kitten = 0; kitten < 20; kitten++){
@@ -24,3 +24,35 @@ for(let kitten = 0; kitten < 20; kitten++){
   	console.log("...human...why you taking pictures of me?...")
   }
 }
+
+//CAN NOT GET THIS TO WORK RIGHT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! FizzBuzz wouldn't log =, (
+for(i = 0; i <= 100; i++){
+	if(i % 3 === 0){
+		console.log('Fizz')
+	}else if(i % 5 ===0){
+		console.log('Buzz')
+	}else if(i % 3 === 0 && i % 5 === 0){
+		console.log('FizzBuzz')
+	}else{
+		console.log(i)
+	}
+}
+*/
+
+const kenny = ["Kenny", 1000, "Austin"];
+const jimHaff = ["Jim H", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const jimClark = ["Jim C", 186, "LA"];
+const ryan = ["Ryan", 65, "Denver"];
+
+
+kenny[0] = 'GameBoy'
+jimClark[1] = 187
+ryan[2] = 'Gotham City'
+reuben[2] = 'Chicago'
+
+jimHaff.splice(2, 1, 'Miami', 'New York', 'Detroit')
+jimHaff.splice(4, 1)
+
+console.log(jimHaff)
+
